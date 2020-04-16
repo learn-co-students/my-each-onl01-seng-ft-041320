5e1dcdfeac758d8a5ba7b87e79da064521e8afc9
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-16 19:04:59 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
-end
+def my_each(arr) # Declare method
+#   # Set initial iteration value
+   i = 0
+#   # Set condition for loop
+   while i < arr.length
+#     # Will yield the specific iteration to a seperate block
+     yield(arr[i])
+     # Increments up by one
+     i += 1
+   end
+   # Returns the array.
+    arr
+ end
