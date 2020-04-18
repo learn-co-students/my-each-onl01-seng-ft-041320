85074cdf2df3a09af51c11a237ed9bf72ff03db6
Chibuisi Enyia <enyia21@gmail.com>
2020-04-18 09:32:57 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
+def my_each(array)
   # code here
+  # 1. Code should start with whether a block exists
+  # 2. Intialize then loop through the array
+  # 3. yeild each element of the array
+  # 4. return the array (returning  the array is that #each does!)
+  if block_given?
+    i = 0
+
+    while (i<array.length)
+      yield (array[i])
+      i+=1
+    end
+  else
+    puts "Block not provided!"
+  end
 end
