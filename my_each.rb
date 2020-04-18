18840436a8d60004785e148ac9012e4bacab1465
Devone Austin <devone.austin@gmail.com>
2020-04-18 01:48:22 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+require 'pry'
+
+#fruit_basket = ["mango", "peach", "orange", "apple"]
+
+def my_each(array)
+    n = 0
+    while n < array.length
+      yield (array[n])
+      n = n + 1
+    end
+    array
 end
+
+#my_each(array)
