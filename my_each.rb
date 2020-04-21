f4fa7c53d379c90e73acab14f7d60c82e171663c
Mercedes Dorsey <dorseymercedes@gmail.com>
2020-04-21 21:59:58 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+collection = [1, 2, 3, 4]
+
+def my_each(arr)
+  i = 0
+  while i < arr.length
+   yield(arr[i])
+  i += 1
 end
+
+collection = (1..100).to_a
+my_each(collection){ |i| puts i}
